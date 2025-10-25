baeb3cc524eef31879f7470ac78dc68d1aa9820e
heywirdgirl <awsmydream@gmail.com>
2025-10-25 10:39:29 +0700

    1039

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { AuthProvider } from "@/contexts/auth-context"; // Import AuthProvider
 import { TimerProvider } from "@/contexts/timer-context";
 import { HistoryProvider } from "@/contexts/history-context";
 import { CycleProvider } from "@/contexts/cycle-context";
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
-    <CycleProvider> {/* Đặt ngoài cùng */}
-      <TimerProvider>
-        <HistoryProvider>
-          {children}
-        </HistoryProvider>
-      </TimerProvider>
-    </CycleProvider>
+    <AuthProvider> {/* Đặt ngoài cùng */}
+      <CycleProvider>
+        <TimerProvider>
+          <HistoryProvider>
+            {children}
+          </HistoryProvider>
+        </TimerProvider>
+      </CycleProvider>
+    </AuthProvider>
   );
 }
