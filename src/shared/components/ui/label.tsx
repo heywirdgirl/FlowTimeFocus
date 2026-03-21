da666de993cec16cc3a37745b4eb08bbf629d001
Heywirdgirl <awsmydream@gmail.com>
2026-03-21 08:18:32 +0000

    1518

--- src/shared/components/ui/label.tsx
+++ src/shared/components/ui/label.tsx
@@ -1,22 +1,15 @@
 "use client"
 
 import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/shared/lib/utils"
-
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
 
 const Label = React.forwardRef<
   HTMLLabelElement,
-  React.LabelHTMLAttributes<HTMLLabelElement> &
-    VariantProps<typeof labelVariants>
+  React.LabelHTMLAttributes<HTMLLabelElement>
 >(({ className, ...props }, ref) => (
   <label
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70", className)}
     {...props}
   />
 ))
